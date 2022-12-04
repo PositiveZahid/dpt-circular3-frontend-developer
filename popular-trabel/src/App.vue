870d9b83c2d3f6fb6a04b0424268892fcc20ea9a
hasanrana21 <hasanrana178821@gmail.com>
2022-12-04 19:13:40 +0600

    initial setup and add tailwindcss

---
+++ popular-trabel/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-4xl text-red-500">this is vue app</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style></style>
