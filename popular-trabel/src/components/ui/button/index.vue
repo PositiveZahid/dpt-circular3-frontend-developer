5fa3846d729ed291bdb5b27dff3da10a72291665
hasanrana21 <hasanrana178821@gmail.com>
2022-12-05 22:16:12 +0600

    dynamic table list from response

---
+++ popular-trabel/src/components/ui/button/index.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <button class="bg-blue-900 px-8 py-2 rounded text-white mb-7">
+    {{ label }}
+  </button>
+</template>
+<script>
+export default {
+  name: "ui-button",
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: "",
+    },
+  },
+};
+</script>
+<style lang=""></style>
--- popular-trabel/src/views/home-page/index.vue
+++ popular-trabel/src/views/home-page/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full mx-auto max-w-md px-2 py-16 sm:px-0">
+  <div class="w-full max-w-md px-2 py-16 sm:px-0">
     <TabGroup>
       <TabList class="flex rounded border-2 border-blue-900">
         <Tab
@@ -12,7 +12,7 @@
             :class="[
               'w-full border-r-2 border-blue-900 last:border-none py-2.5 text-sm font-medium leading-5 text-blue-700',
               '',
-              selected ? 'bg-blue-600 text-white' : '',
+              selected ? 'bg-blue-900 text-white outline-none' : '',
             ]"
           >
             {{ item }}
@@ -21,7 +21,75 @@
       </TabList>
 
       <TabPanels class="mt-2">
-        <TabPanel>this is tab panel1 </TabPanel>
+        <TabPanel>
+          <ui-button label="Search" @click="fetchData"></ui-button>
+          <ui-table :headers="state.headers">
+            <tr
+              v-for="(item, key) in state.data"
+              :key="key"
+              class="border-b border-gray-300 hover:bg-primary-3/10 transition-all"
+            >
+              <td class="table-data">
+                <span
+                  v-for="(segment, key) in getItineraries(item.itineraries[0])"
+                  :key="key"
+                  class="block"
+                  >{{
+                    segment?.carrierCode + " " + segment?.flightNumber
+                  }}</span
+                >
+              </td>
+              <td class="table-data">
+                <span
+                  v-for="(segment, key) in getItineraries(item.itineraries[0])"
+                  :key="key"
+                  class="block"
+                >
+                  {{ segment?.aircraft }}
+                </span>
+              </td>
+              <td class="table-data">
+                <span
+                  v-for="(item, index) in item.class[0]"
+                  :key="index"
+                  class="block"
+                  >{{ item }}</span
+                >
+              </td>
+              <td class="table-data">
+                <span
+                  v-for="(flight, index) in item.fareBasis[0]"
+                  :key="index"
+                  class="block"
+                  >{{ flight }}</span
+                >
+              </td>
+              <td class="table-data">afdasdf</td>
+              <td class="table-data">
+                <span
+                  v-for="(segment, key) in getItineraries(item.itineraries[0])"
+                  :key="key"
+                  class="block"
+                  >{{ segment?.departure?.at }}</span
+                >
+              </td>
+              <td class="table-data">
+                <span
+                  v-for="(segment, key) in getItineraries(item.itineraries[0])"
+                  :key="key"
+                  class="block"
+                  >{{ segment?.arrival?.at }}</span
+                >
+              </td>
+              <td class="table-data">
+                {{ item.itineraries[0]?.duration }}
+              </td>
+              <td class="table-data">
+                {{ item.price }}
+              </td>
+            </tr>
+          </ui-table>
+        </TabPanel>
         <TabPanel>this is tab panel2 </TabPanel>
         <TabPanel>this is tab panel3 </TabPanel>
       </TabPanels>
@@ -32,14 +100,76 @@
 <script>
 import { reactive } from "vue";
 import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
+import UiButton from "../../../src/components/ui/button/index.vue";
+import response from "../../../src/components/data/responseData";
+import UiTable from "../../../src/components/ui/table/index.vue";
 export default {
-  components: { TabGroup, TabList, Tab, TabPanels, TabPanel },
+  components: {
+    TabGroup,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel,
+    UiButton,
+    UiTable,
+  },
   setup() {
     const state = reactive({
       tabs: ["One Way", "Round Trip", "Multi City"],
+      headers: [
+        {
+          label: "Flight",
+        },
+        {
+          label: "Aircraft",
+        },
+        {
+          label: "Class",
+        },
+        {
+          label: "Fare",
+        },
+        {
+          label: "Route",
+        },
+        {
+          label: "Departure",
+        },
+        {
+          label: "Arrival",
+        },
+        {
+          label: "Duration",
+        },
+        {
+          label: "Price",
+        },
+      ],
+      data: [],
     });
 
-    return { state };
+    const fetchData = () => {
+      console.log("fetchData", response.flightOffer);
+      state.data = response.flightOffer;
+    };
+
+    const getItineraries = (item) => {
+      // console.log("item", item);
+      let segmented = [];
+      item?.segments.map((segment) => {
+        segmented.push(segment);
+      });
+      console.log("seg", segmented);
+      return segmented;
+    };
+
+    return { state, fetchData, getItineraries };
   },
 };
 </script>
+
+<style scoped>
+.table-data {
+  @apply text-lg font-normal px-[30px] py-3.5;
+}
+</style>
