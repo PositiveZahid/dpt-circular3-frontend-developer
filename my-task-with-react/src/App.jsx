0523ef2669419e4e5f855f1e30c2ae8850e9e02f
Hasibul294 <hasibulhasan294@gmail.com>
2024-05-10 22:39:27 +0600

    create new project with react and install tailwind for designing client component also intall react-router-dom

---
+++ my-task-with-react/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-300">
+        This is my project with react and vite
+      </h1>
+    </>
+  );
+}
+
+export default App;
